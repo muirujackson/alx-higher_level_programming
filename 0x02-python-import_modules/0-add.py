2b59c992f18a02c8458c63681a0ce710fb6acfe7
muirujackson <jakezweru@gmail.com>
2023-06-08 15:20:44 -0700

    added a and b variables

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-import add_0
 if __name__ == "__main__":
-    print("{} + {} = {}".format(1, 2, add_0.add(1, 2)))
+    import add_0
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
