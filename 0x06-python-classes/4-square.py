eacab5d5f56ab3fa92b26e8bfcbcf8615b625ff1
muirujackson <jakezweru@gmail.com>
2023-06-28 16:30:56 -0700

    modified quiz 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 
-def list_division(my_list_1, my_list_2, list_length):
-    """Divides two lists element by element.
+"""Define a class Square."""
 
-    Args:
-        my_list_1 (list): The first list.
-        my_list_2 (list): The second list.
-        list_length (int): The number of elements to divide.
 
-    Returns:
-        A new list of length list_length containing all the divisions.
-    """
-    new_list = []
-    for i in range(0, list_length):
-        try:
-            div = my_list_1[i] / my_list_2[i]
-        except TypeError:
-            print("wrong type")
-            div = 0
-        except ZeroDivisionError:
-            print("division by 0")
-            div = 0
-        except IndexError:
-            print("out of range")
-            div = 0
-        finally:
-            new_list.append(div)
-    return (new_list)
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
