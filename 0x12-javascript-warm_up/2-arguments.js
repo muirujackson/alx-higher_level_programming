310939d2610e0d32fb53270104ff18c813160bb8
muirujackson <jakezweru@gmail.com>
2023-09-11 09:25:13 -0700

    process argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const len = process.argv.length;
+if (len === 2)
+	console.log('No argument');
+if (len === 3)
+	console.log('Argument found');
+if (len > 3)
+	console.log('Arguments found');
