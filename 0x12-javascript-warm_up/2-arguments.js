495c6c4a3acef52c5e2f14587c3072c811c5aa8b
muirujackson <jakezweru@gmail.com>
2023-09-12 04:51:48 -0700

    revised q2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 const len = process.argv.length;
-if (len === 2)
-	console.log('No argument');
-if (len === 3)
-	console.log('Argument found');
-if (len > 3)
-	console.log('Arguments found');
+if (len === 2) {
+  console.log('No argument');
+}
+if (len === 3) {
+  console.log('Argument found');
+}
+if (len > 3) {
+  console.log('Arguments found');
+}
