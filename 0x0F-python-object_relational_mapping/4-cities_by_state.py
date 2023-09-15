0673f7c7e03c8236559892fd56e5fd1b33829c7f
muirujackson <jakezweru@gmail.com>
2023-09-15 07:26:10 -0700

    used join

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,8 +12,9 @@ if __name__ == "__main__":
         host="localhost", port=3306, user=argv[1],
         password=argv[2], database=argv[3])
     cursor = conn.cursor()
-    cursor.execute("SELECT C.id, C.name, S.name FROM cities AS C,\
-            states AS S WHERE C.state_id = S.id ORDER BY C.id ASC")
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+            FROM cities JOIN states ON cities.state_id \
+            = states.id ORDER BY cities.id ASC")
     rows = cursor.fetchall()
 
     if rows_selected is not None:
