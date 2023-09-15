5f6ec440ec31234fac3b898111898bfedc23fee1
muirujackson <jakezweru@gmail.com>
2023-09-15 06:14:37 -0700

    using % is safer

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
             charset="utf8"
             )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-            ORDER BY id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s\
+            ORDER BY id ASC", (argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
