67a22d1ed1cf5cfa07ba71d4662b8279012b5224
muirujackson <jakezweru@gmail.com>
2023-09-15 02:16:20 -0700

    case sensitive with Binary

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
             charset="utf8"
             )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}'\
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}'\
             ORDER BY id ASC".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
