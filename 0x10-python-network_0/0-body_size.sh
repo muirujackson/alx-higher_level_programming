c8a20925f450b02bc60fa0b670f40ee800631de2
muirujackson <jakezweru@gmail.com>
2023-09-28 10:23:55 -0700

    count size of response body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
+# A script that takes in a URL, sends a request to that URL
 curl -s "$1" | wc -c
