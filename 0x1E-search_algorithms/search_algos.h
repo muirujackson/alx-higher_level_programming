23d46f8a16dbbf64820da98f1d0cfa7eb9378e64
muirujackson <jakezweru@gmail.com>
2023-10-09 13:06:28 -0700

    added header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGO
+#define SEARCH_ALGO
+
+/* header */
+#include stdio.h
+
+/* prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
